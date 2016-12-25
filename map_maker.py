bea87add6b45e5e3bd2208b76f0aa8fdb8325f87
Malcolm <malcolmbalch@gmail.com>
2016-12-25 17:52:00 -0600

    Save test output by repo/branch

--- map_maker.py
+++ map_maker.py
@@ -1,7 +1,14 @@
+import os
 import re
+import json
+import nose
+import coverage
 import subprocess
+import hashlib
+from glob import glob
 from file_observer import MapMakerObserver
 from filewatch import file_updated_subject, Watcher
+from git import Repo
 
 class MapMaker:
     
@@ -17,12 +24,10 @@ class MapMaker:
     def __init__(self, watch_paths, delta_threshold):
         # Initialize observers for each watch_path
         for path in watch_paths:
-            # TODO: Put this in its own function since it's going to be called from file and branch change
-
             # Build initial maps
+
             # TODO: Check for map existence/branch/change delta
-            tests = self.collect_tests(path)
-            self.build_map(path, tests)
+            self._make_test_map_current(path)
 
             # Initialize file watchers
             obs = MapMakerObserver(self, path)
@@ -32,15 +37,14 @@ class MapMaker:
         watcher = Watcher()
         watcher.run()
 
-    def branch_change(self, watch_path):
-        """ Called from file observer on git branch change"""
-        print "Branch change: "+watch_path
+    def _make_test_map_current(self, path):
+        """ Create or ovewrite map data for a repository's current branch """
+        tests = self._collect_tests(path)
+        repo = Repo(path)
+        branch = repo.active_branch.name
+        self._build_map(path, tests, branch)
 
-    def file_change(self, watch_path):
-        """ Called from file observer on file change without git branch change"""
-        print "file in directory changed: "+watch_path
-
-    def collect_tests(self, watch_path):
+    def _collect_tests(self, watch_path):
         """ Gets a list of tests to run """        
         
         # nose prints to stderr
@@ -48,17 +52,49 @@ class MapMaker:
         _, test_output= p.communicate()
         lines = test_output.split('\n')
         nose_args = self._format_test_paths(lines)
+        return nose_args
     
-    def build_map(self, watch_path, test_list):
-        """ Builds a test map for a watch path """
-        pass
+    def _build_map(self, watch_path, test_list, branch):
+        """ Builds a test map for a watch path/git branch combination """
+        test_map = {} 
+        
+        # List of project files 
+        file_list =  [y for x in os.walk(watch_path) for y in glob(os.path.join(x[0], '*.py'))]
+        
+        for path in test_list:
+            cov = coverage.Coverage()
+            cov.start()
+            test_path = path[0]+'.py:'+path[1]
+            result = nose.run(argv=['--nocapture', watch_path+'/'+test_path])
+            cov.stop()
+            cov.save()
 
-    def get_change_delta(self, watch_path):
+            # For each project file
+            for file_name in file_list:
+                line_numbers_covered = cov.data.lines(file_name)
+                if line_numbers_covered:
+                    if file_name not in test_map:
+                        test_map[file_name] = {}
+                    for line in line_numbers_covered:
+                        if line not in test_map[file_name]:
+                            test_map[file_name][line] = []
+                        test_map[file_name][line].append(test_path)
+        
+        # Save the test map data in a directory referencing its repo and branch
+        watch_path_hash = hashlib.md5(watch_path).hexdigest()
+        branch_hash = hashlib.md5(branch).hexdigest()
+        if not os.path.isdir('.map_data'):
+            os.mkdir('.map_data')
+        if not os.path.isdir('.map_data/'+watch_path_hash):
+            os.mkdir('.map_data/'+watch_path_hash)
+        if not os.path.isdir('.map_data/'+watch_path_hash+'/'+branch_hash):
+            os.mkdir('.map_data/'+watch_path_hash+'/'+branch_hash)
+
+        with open('.map_data/'+watch_path_hash+'/'+branch_hash+'/test_map.json', 'w') as fp:
+            json.dump(test_map, fp)
+
+    def _get_change_delta(self, watch_path):
         """ Returns the % change of code since the last map """
-        pass
-
-    def poll_paths(self):
-        """ Runs an infinite loop checking for changes """
         pass
 
     def _format_test_paths(self, nose_output):
@@ -73,3 +109,11 @@ class MapMaker:
                     dot_path = class_name+'.'+test_name
                     nose_args.append((test_file_path, dot_path))
             return nose_args 
+
+    def branch_change(self, watch_path):
+        """ Called from file observer on git branch change"""
+        print "Branch change: "+watch_path
+
+    def file_change(self, watch_path):
+        """ Called from file observer on file change without git branch change"""
+        print "file in directory changed: "+watch_path
